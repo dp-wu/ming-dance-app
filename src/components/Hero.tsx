bfa5f83e26721e8bc9e41bea77e987dbfad8d22a
Yvette <dp-wu@users.noreply.github.com>
2025-08-19 14:14:47 -0700

    remove subtitle for header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,6 @@ export default function Hero() {
     return (
         <div className="hero">
             <h1>Ming Dance</h1>
-            <p className="subtitle">To heal with knowledge  To move with intention</p>
             <Navbar />
         </div>
     );
